2099faeff6d77a94a2139af83815e632e9e5d270
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-10-01 17:46:18 +0200

    create Products component

---
+++ resources/js/components/ProductItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="card" style="width: 16rem;">
+    <img v-bind:src="featuredPhoto" class="card-img-top" alt="photo">
+    <div class="card-body">
+        <h5 class="card-title">{{title}}</h5>
+        <StarRating v-bind:rate="rate" v-bind:reviewsCount="reviewsCount"></StarRating>
+        <hr>
+        <div class="price-section">
+            <span class="price">${{price}}</span>
+            <button class="btn btn-primary btn-custom">ADD TO CART</button>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import StarRating from './StarRating';
+
+export default {
+    props: {
+        title: String,
+        price: Number,
+        featuredPhoto: String,
+        rate: String,
+        reviewsCount: Number
+    },
+    components: {
+        StarRating
+    }
+}
+</script>
+
+<style scoped>
+
+    .card {
+        box-shadow: 1px 2px 6px #ccc;
+        margin-bottom: 15px;
+    }
+
+    .card-img-top {
+        height: 170px;
+    }
+
+    .price-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .price {
+        font-weight: bold;
+    }
+
+    .btn-custom {
+        font-size: 12px;
+    }
+</style>
---
+++ resources/js/components/Products.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+            <!-- <div v-for="product in products" v-bind:key="product.id">
+               <h3>{{product.name}}</h3>
+            </div> -->
+
+            <div class="col-md-3" v-for="product in products" v-bind:key="product.id">
+                <ProductItem 
+                    v-bind:title="product.name"
+                    v-bind:price="product.price"
+                    v-bind:featuredPhoto="product.featuredPhoto"
+                    v-bind:rate="product.rate"
+                    v-bind:reviewsCount="product.reviewsCount"
+                ></ProductItem>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import ProductItem from './ProductItem';
+
+export default {
+
+    data() {
+        return {
+            products: [],
+            product: ''
+        }
+    },
+
+    components: {
+        ProductItem
+    },
+
+    methods: {
+        getProducts() {
+        axios
+            .get('api/products')
+            .then(res => {this.products = res.data})
+            .catch(err => console.log(err));
+        }
+    },
+
+    created() {
+        this.getProducts();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
