88b948e28ddd9a1123dfa145d7f640da3f79daf6
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-10-02 12:33:43 +0200

    include app component in app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,10 @@ require('./bootstrap');
 window.Vue = require('vue');
 Vue.config.productionTip = false
 
-Vue.component('navbar-vue', require('./components/Navbar.vue').default);
-
+// Vue.component('navbar-vue', require('./components/Navbar.vue').default);
+// Vue.component('login-modal-vue', require('./components/LoginModal.vue').default);
+// Vue.component('products-vue', require('./components/Products.vue').default);
+Vue.component('app-vue', require('./components/App.vue').default);
 
 const app = new Vue({
     el: '#app'
